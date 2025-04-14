174261cd05421da4107357fabc7a2f986e017e51
Muhammad Adeel <129621294+Adeel-116@users.noreply.github.com>
2025-04-14 08:07:12 +0500

    third commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import HomePage from './pages/HomePage'
 import {useState, useEffect} from 'react'
 import LoadingScreen from './components/LoadingScreen'
 function App() {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         setLoading(true)
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             setLoading(false)
-        }, 2000)
-    }, [])
+        }, 3000)
 
+        return () => clearTimeout(timer) 
+    }, [])
 
 
   return (
